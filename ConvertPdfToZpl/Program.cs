78f130654801b2e1f448eea73d2fc4ac769aa76b
gökanıl <gokhan.anilk@gmail.com>
2021-11-21 10:07:36 +0300

    First commit.

---
+++ ConvertPdfToZpl/Program.cs
@@ -0,0 +1,32 @@
+﻿using PDFtoZPL;
+using System;
+using System.IO;
+
+namespace ConvertPdfToZpl
+{
+    internal class Program
+    {
+        //Execute single file: dotnet publish -r win-x86 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
+        static void Main(string[] args)
+        {
+            if (args?.Length == 0)
+                return;
+
+            string filePath = args[0];
+            string fileName = $"{Path.GetFileNameWithoutExtension(filePath)}.zpl";
+
+            Console.WriteLine($"Converting to zpl -> {fileName}");
+
+            try
+            {
+                byte[] file = File.ReadAllBytes(filePath);
+                string zpl = Conversion.ConvertPdfPage(file, dpi: 300);
+                File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), fileName), zpl);
+            }
+            catch
+            {
+                Console.WriteLine("Convert failed.");
+            }
+        }
+    }
+}
